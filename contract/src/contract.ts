2a6bce29d94888d8549391a63950af3a3589c5e2
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2023-05-03 15:35:48 +0300

    My 1st NEAR contract on typescript

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -3,4 +3,42 @@ import { NearBindgen, near, call, view } from 'near-sdk-js';
 @NearBindgen({})
 class Contract {
   
+  counter: number = 0;
+
+  @view({})
+  getCounter(): number {
+    return this.counter;
+  }
+
+  @call({})
+  increaseCounter(): void {
+    near.log(`Inc counter by ${near.predecessorAccountId()}`);
+    this.counter++;
+  }
+
+  @call({})
+  dereaseCounter(): void {
+    if (this.counter == 0) {
+      throw new Error("Can't decrease counter");
+    }
+
+    near.log(`Dec counter by ${near.predecessorAccountId()}`);
+    this.counter--;
+  }
+
+  @call({ privateFunction: true })
+  resetCounter(): void {
+    this.counter = 0;
+  }
+
+  @call({ payableFunction: true })
+  setCounter(value: number): void {
+    if (near.attachedDeposit() < 1e22) {
+      throw Error("Deposit amount should be >= 0.01 NEAR");
+    }
+    near.log(`Deposit amount ` + near.attachedDeposit());
+
+    this.counter = value;
+  }
+
 }
